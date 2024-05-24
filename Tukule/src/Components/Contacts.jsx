f76f9cd19ca8b60814e777ced4d0f8ca02cafc04
Brandon-Datch <135508818+Brandon-Datch@users.noreply.github.com>
2024-05-24 10:44:35 +0300

    Created components

---
+++ Tukule/src/Components/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+      <h2>Contact Section</h2>
+      <p>Add your contact information here.</p>
+    </div>
+  );
+};
+
+export default Contacts;
