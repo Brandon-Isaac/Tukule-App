72b1dc2e6a1865cd6a633eaaedce497637e592e9
Brandon-Datch <135508818+Brandon-Datch@users.noreply.github.com>
2024-05-24 11:04:45 +0300

    Updated App.jsx

--- Tukule/src/App.jsx
+++ Tukule/src/App.jsx
@@ -5,7 +5,7 @@ import { Header } from "./Components/Header";
 import { Footer } from "./Components/Footer";
 import { Home } from "./Components/Home";
 import { About } from "./Components/About";
-import { Contact } from "./Components/Contact";
+import { Contacts } from "./Components/Contacts";
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
         <Header />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contacts />} />
         <Footer />
       </Routes>
     </Router>
