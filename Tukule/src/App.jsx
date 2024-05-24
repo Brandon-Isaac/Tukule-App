9c5ec1d8c93c40da7b8913e4ff0e178419e08178
Brandon-Datch <135508818+Brandon-Datch@users.noreply.github.com>
2024-05-24 11:01:58 +0300

    Updated App.jsx

--- Tukule/src/App.jsx
+++ Tukule/src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
 import { Header } from "./Components/Header";
 import { Footer } from "./Components/Footer";
+import { Home } from "./Components/Home";
+import { About } from "./Components/About";
+import { Contact } from "./Components/Contact";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Header />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Footer />
+      </Routes>
+    </Router>
   );
 };
 export default App;
