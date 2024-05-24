0d59c2299437fd81cc751e86cbfb86c58dcbf530
Brandon-Datch <135508818+Brandon-Datch@users.noreply.github.com>
2024-05-24 13:03:58 +0300

    Semi-finished header

--- Tukule/src/Components/Header.jsx
+++ Tukule/src/Components/Header.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from "../../public/tupike.png";
+import logo from "../../public/logo.png";
 
 export const Header = () => {
   return (
-    <header>
-      <div className="logo">
-        <img src={logo} alt="TUPIKE LOGO" />
+    <header className="header">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="TUPIKE LOGO" />
+        </div>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
